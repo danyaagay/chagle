2218e5d96fa2736a37189ab889aa93c9011f3838
danyaagay <danniels.krouser@gmail.com>
2023-10-17 19:57:02 +0700

    Roles and seeder super-admin add

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'email_verified_at' => $this->email_verified_at,
+            'roles' => $this->getRoleNames(),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        //$role = Role::create(['name' => 'super-admin']);
+        //$role->givePermissionTo(Permission::all());
+
+        $user = \App\Models\User::factory()->create([
+            'name'     => 'Admin',
+            'email'    => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+        $user->assignRole('super-admin');
     }
 }
